7907369055f748a0692e8bc5d46998e1a87c3c45
AnneKsatn <AnneKsatn@gmail.com>
2021-08-15 16:14:45 +0300

    UI for pastures

---
+++ admin-app/src/app/system/infrastructure/pastures/pastures.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PasturesComponent } from './pastures.component';
+
+describe('PasturesComponent', () => {
+  let component: PasturesComponent;
+  let fixture: ComponentFixture<PasturesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PasturesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PasturesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
